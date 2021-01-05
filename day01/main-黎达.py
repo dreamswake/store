b64e3ecff902717b6277ccd2b60aee1b0ada525a
“lida” <710266001@qq.com>
2021-01-05 20:45:22 +0800

    第一天

---
+++ day01/main-黎达.py
@@ -0,0 +1,130 @@
+print("hello world!")
+print("------------------------欢迎来到新鲜水果商店--------------------")
+print("=============================================================")
+print("水果名称            水果价格           水果品质           水果数量")
+print("-------------------------------------------------------------")
+print("苹果                 0.5              A                 100")
+print("橘子                 0.2              B                 500")
+print("橙子                 0.6              B                 200")
+print("雪梨                 0.4              A                 300")
+print("香蕉                 1.0              A                 400")
+print("火龙果               3.0              A+                500")
+print("榴莲                 100              A+                100")
+print("车厘子               5.0              A+                500")
+print("凤梨                 15.0             A                 100")
+print("草莓                 3.0              A+                100")
+print("山竹                 8.0              A                  50")
+print("龙眼                 2.0              A                 100")
+print("哈密瓜               15.0             A                  30")
+print("柠檬                 3.0              A                 50")
+print("葡萄                 0.5              A                 500")
+print("椰子                 10               A                 100")
+print("波罗蜜               200              A                 100")
+print("猕猴桃               3.0              A                 100")
+print("甘蔗                 8.0              A                 100")
+print("柚子                 20.0             A                 100")
+print("-------------------------------------------------------------")
+print("总金额为:",(0.5*100+0.2*500+0.6*200+0.4*300+1.0*400+3.0*500+100*100+5.0*500+15*100+3*100+8*50+2*100+15*30+3*50+0.5*500+10*100+200*100+3*100+8*100+20*100),"￥")
+
+name1="苹果"
+price1=0.5
+quality1="A"
+num1=100
+name2="橘子"
+price2=0.2
+quality2="B"
+num2=500
+name3="橙子"
+price3=0.6
+quality3="B"
+num3=200
+name4="雪梨"
+price4=0.4
+quality4="A"
+num4=300
+name5="香蕉"
+price5=1.0
+quality5="A"
+num5=400
+name6="火龙果"
+price6=3.0
+quality6="A+"
+num6=500
+name7="榴莲"
+price7=100
+quality7="A+"
+num7=100
+name8="车厘子"
+price8=5.0
+quality8="A+"
+num8=500
+name9="凤梨"
+price9=15.0
+quality9="A"
+num9=100
+name10="草莓"
+price10=3.0
+quality10="A+"
+num10=100
+name11="山竹"
+price11=8.0
+quality11="A+"
+num11=50
+name12="龙眼"
+price12=2.0
+quality12="A"
+num12=100
+name13="哈密瓜"
+price13=15.0
+quality13="A"
+num13=30
+name14="柠檬"
+price14=3.0
+quality14="A"
+num14=50
+name15="葡萄"
+price15=0.5
+quality15="A"
+num15=500
+name16="椰子"
+price16=10
+quality16="A"
+num16=100
+name17="波罗蜜"
+price17=200
+quality17="A"
+num17=100
+name18="猕猴桃"
+price18=3
+quality18="A"
+num18=100
+name19="甘蔗"
+price19=8
+quality19="A"
+num19=100
+name20="柚子"
+price20=20
+quality20="A"
+num20=100
+total=(price1*num1+price2*num2+price3*num3+price4*num4+price5*num5+price6*num6+price7*num7+price8*num8+price9*num9+price10*num10+price11*num11+price12*num12+price13*num13+price14*num14+price15*num15+price16*num16+price17*num17+price18*num18+price19*num19+price20*num20)
+print(name1,"\t\t",price1,"\t\t",quality1,"\t\t",num1,"\t\t")
+print(name2,"\t\t",price2,"\t\t",quality2,"\t\t",num2,"\t\t")
+print(name3,"\t\t",price3,"\t\t",quality3,"\t\t",num3,"\t\t")
+print(name4,"\t\t",price4,"\t\t",quality4,"\t\t",num4,"\t\t")
+print(name5,"\t\t",price5,"\t\t",quality5,"\t\t",num5,"\t\t")
+print(name6,"\t\t",price6,"\t\t",quality6,"\t",num6,"\t\t")
+print(name7,"\t\t",price7,"\t\t",quality7,"\t",num7,"\t\t")
+print(name8,"\t\t",price8,"\t\t",quality8,"\t",num8,"\t\t")
+print(name9,"\t\t",price9,"\t\t",quality9,"\t\t",num9,"\t\t")
+print(name10,"\t\t",price10,"\t\t",quality10,"\t",num10,"\t\t")
+print(name11,"\t\t",price11,"\t\t",quality11,"\t",num11,"\t\t")
+print(name12,"\t\t",price12,"\t\t",quality12,"\t\t",num12,"\t\t")
+print(name13,"\t\t",price13,"\t\t",quality13,"\t\t",num13,"\t\t")
+print(name14,"\t\t",price14,"\t\t",quality14,"\t\t",num14,"\t\t")
+print(name15,"\t\t",price15,"\t\t",quality15,"\t\t",num15,"\t\t")
+print(name16,"\t\t",price16,"\t\t",quality16,"\t\t",num16,"\t\t")
+print(name17,"\t\t",price17,"\t\t",quality17,"\t\t",num17,"\t\t")
+print(name18,"\t\t",price18,"\t\t\t",quality18,"\t\t",num18,"\t\t")
+print(name19,"\t\t",price19,"\t\t\t",quality19,"\t\t",num19,"\t\t")
+print(name20,"\t\t",price20,"\t\t",quality20,"\t\t",num20,"\t\t")
+print("总金额为：",(total),"￥")
