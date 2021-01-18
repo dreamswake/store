6cefdcf7e3c11a21376dd8895b031850af3bf4df
“lida” <710266001@qq.com>
2021-01-18 16:39:50 +0800

    删除了day06

--- day06/homework/work1.1.py
+++
@@ -1,5 +0,0 @@
-from homework.work1 import Test
-test = Test()
-num1 = int(input("请输入数字："))
-num2 = int(input("请输入数字："))
-test.Add(num1,num2)
--- day06/homework/work2.1.py
+++
@@ -1,8 +0,0 @@
-from homework.work2 import oldPhone
-from homework.work2 import newPhone
-print("旧手机")
-phone = oldPhone("诺基亚")
-phone.call("110")
-print("新手机")
-phone = newPhone("诺基亚")
-phone.call(110)
